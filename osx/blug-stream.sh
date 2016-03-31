73b12e6843ac326cef1168236cce8d2c968b040d
Bård Aase <baa@vizrt.com>
2016-03-31 10:34:29 +0200

    exit debug mode

--- osx/blug-stream.sh
+++ osx/blug-stream.sh
@@ -6,4 +6,4 @@ read -p "source video device index > " SCREEN
 read -p "source audio device index > " AUDIO
 read -p "target host:port > " HOSTPORT
 
-echo $FFMPEG -f avfoundation -i "${SCREEN}:${AUDIO}" -an -vcodec mpeg2video -b:v 5000k -f mpegts "udp://${HOSTPORT}"
+$FFMPEG -f avfoundation -i "${SCREEN}:${AUDIO}" -an -vcodec mpeg2video -b:v 5000k -f mpegts "udp://${HOSTPORT}"
