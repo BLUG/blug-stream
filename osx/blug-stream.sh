a335c220f18a534ae45d263bc0e3c7c2de30c38f
Bård Aase <baa@vizrt.com>
2016-03-31 10:37:32 +0200

    increate bitrate

--- osx/blug-stream.sh
+++ osx/blug-stream.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -v
-FFMPEG="./ffmpeg"
+FFMPEG="$(dirname $0)/ffmpeg"
+BITRATE="8000k"
 $FFMPEG -f avfoundation -list_devices true -i ""
 read -p "source video device index > " SCREEN
 read -p "source audio device index > " AUDIO
 read -p "target host:port > " HOSTPORT
 
-$FFMPEG -f avfoundation -i "${SCREEN}:${AUDIO}" -an -vcodec mpeg2video -b:v 5000k -f mpegts "udp://${HOSTPORT}"
+$FFMPEG -f avfoundation -i "${SCREEN}:${AUDIO}" -an -vcodec mpeg2video -b:v $BITRATE -f mpegts "udp://${HOSTPORT}"
