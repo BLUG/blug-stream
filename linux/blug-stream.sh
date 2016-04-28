1baedfedea1fee768997a3187f005bb516c6b37b
Bård Aase <elzapp@gmail.com>
2016-04-28 13:42:53 +0200

    added stream script for linux

---
+++ linux/blug-stream.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -v
+
+if [ ! -f "ffmpeg" ]
+then
+  wget https://github.com/BLUG/blug-stream/raw/ddd65038c6828d86f87177225db9d04eb15f3e68/linux/ffmpeg
+fi
+
+BITRATE="8000k"
+read -p "target host:port > " HOSTPORT
+getResolution(){
+  xrandr|sed -n 's/\(.*\) connected primary \([0-9]*\)x\([0-9]*\)+.*/\2x\3/p'|head -n 1
+}
+getResolution
+./ffmpeg -video_size $(getResolution) -framerate 25 -f x11grab -i :1.0+0,0 \
+  -video_size 1280x720 -an -vcodec mpeg2video -b:v $BITRATE -f mpegts "udp://${HOSTPORT}"
